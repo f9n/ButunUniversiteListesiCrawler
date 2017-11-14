10af2df5a43fdedb9f91e93d1b888d0ad2af549a
pleycpl <h4rvey@protonmail.com>
2017-11-14 22:19:09 +0000

    updating settings.py file with script

---
+++ main.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+MY_LINENO=0
+SETTINGS_PY=test_settings.py
+
+function addLastContent() {
+    echo "]" >> ${SETTINGS_PY}
+}
+
+function addUniversity() {
+    local uni=$1
+    echo -e "\t('${uni}', '${uni}'),"
+}
+
+function addUniversities() {
+    echo "added"
+    while read -r UNI; do
+        #echo "${UNI}"
+        addUniversity "${UNI}" >> ${SETTINGS_PY}
+    done < universities.txt
+}
+
+function addHeadContent() {
+    echo "UNIVERSITIES = [" >> ${SETTINGS_PY}
+}
+
+function clearUp() {
+    head -n "$MY_LINENO" ${SETTINGS_PY} > temp_${SETTINGS_PY}
+    mv temp_${SETTINGS_PY} ${SETTINGS_PY}
+}
+
+function learnLineNo() {
+    MY_LINENO=$(grep -n "UNIVERSITIES" ${SETTINGS_PY} | awk -F ':' '{ print $1}')
+    ((MY_LINENO--))
+}
+
+function main() {
+    learnLineNo
+    clearUp
+    addHeadContent
+    addUniversities
+    addLastContent
+}
+main "$@"
